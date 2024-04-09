f98f7b43177fa2818fd57b9ed16887699ec47df8
Chris Darden <chris.a.darden@gmail.com>
2024-04-08 22:28:31 -0500

    Update app.component.ts, tsconfig.json, app.module.ts, elements.module.ts, and angular.json

---
+++ src/app/elements/ElementsHome/ElementsHome.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-elements-home',
+  templateUrl: './ElementsHome.component.html',
+  styleUrls: ['./ElementsHome.component.css']
+})
+export class ElementsHomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/elements/elements.module.ts
+++ src/app/elements/elements.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ElementsRoutingModule } from './elements-routing.module';
+import { ElementsHomeComponent } from "./ElementsHome/ElementsHome.component";
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ElementsHomeComponent],
   imports: [
     CommonModule,
     ElementsRoutingModule
-  ]
+  ],
+  exports: [ElementsHomeComponent]
 })
 export class ElementsModule { }
