b61fcc656baf2760f9da2773fa5ca3c4bc73f8a0
Chris Darden <chris.a.darden@gmail.com>
2024-04-08 22:01:00 -0500

    Remove unused code files and update configuration files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class CollectionsRoutingModule { }
+export class AppRoutingModule { }
