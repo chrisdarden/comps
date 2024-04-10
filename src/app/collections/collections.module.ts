039c0b4603dea7fabb9e80ad96a7d06cb9325bfb
Chris Darden <chris.a.darden@gmail.com>
2024-04-10 11:29:13 -0500

    Update CollectionsHome component to use app-divider component instead of a paragraph tag, added collections component header

--- src/app/collections/collections.module.ts
+++ src/app/collections/collections.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CollectionsHomeComponent } from './collections-home/CollectionsHome.component';
 import { CollectionsRoutingModule } from './collections-routing.module';
-
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [CollectionsHomeComponent],
   imports: [
     CommonModule,
-    CollectionsRoutingModule
+    CollectionsRoutingModule,
+    SharedModule
   ],
   exports: []
 })
