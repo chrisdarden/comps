7462c2f4ad341cd5270db27cfbce44fe809826b7
Chris Darden <chris.a.darden@gmail.com>
2024-04-20 16:33:21 -0500

    added showable modal with div dark grey background with closable buttons

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,8 @@ const routes: Routes = [
   { path: 'collections', loadChildren: () => import('./collections/collections.module').then(m => m.CollectionsModule)},
   { path: 'elements', loadChildren: () => import('./elements/elements.module').then(m => m.ElementsModule) },
   { path: 'views', loadChildren: () => import('./views/views.module').then(m => m.ViewsModule)},
+  { path: 'modules', loadChildren: () => import('./mods/mods.module').then(m => m.ModsModule)},
+
   { path: '', component: HomeComponent },
   { path: 'home', redirectTo: '', pathMatch: 'full' },
   { path: '**', component: NotFoundComponent }
